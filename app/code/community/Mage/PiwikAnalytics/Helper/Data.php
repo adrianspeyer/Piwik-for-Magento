eb927964b4805b49c7c86bece7188c105b477123
adriansonline <adriansonline@gmail.com>
2012-06-21 19:15:47 -0400

    Full Source code Signed-off-by: adriansonline

---
+++ app/code/community/Mage/PiwikAnalytics/Helper/Data.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * Piwik Extension for Magento created by Adrian Speyer
+ * Get Piwik at http://www.piwik.org - Open source web analytics
+ *
+ * @category    Mage
+ * @package     Mage_PiwikAnalytics_Helper_Data
+ * @copyright   Copyright (c) 2012 Adrian Speyer. (http://www.adrianspeyer.com)
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+
+/**
+ * PiwikAnalytics data helper
+ *
+ * @category   Mage
+ * @package    Mage_PiwikAnalytics
+ */
+class Mage_PiwikAnalytics_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    /**
+     * Config paths for using throughout the code
+     */
+    const XML_PATH_ACTIVE  = 'piwik/analytics/active';
+    const XML_PATH_SITE =    'piwik/analytics/site';
+	const XML_PATH_INSTALL = 'piwik/analytics/install';
+	
+    /**
+     *
+     * @param mixed $store
+     * @return bool
+     */
+    public function isPiwikAnalyticsAvailable($store = null)
+    {
+        $siteId = Mage::getStoreConfig(self::XML_PATH_SITE, $store);
+		//$installPath = Mage::getStoreConfig(self::XML_PATH_INSTALL, $store);
+        //return $siteId && $installPath && Mage::getStoreConfigFlag(self::XML_PATH_ACTIVE, $store);
+		return $siteId && Mage::getStoreConfigFlag(self::XML_PATH_ACTIVE, $store);
+	}
+}
---
+++ app/code/community/Mage/PiwikAnalytics/Model/Observer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * Piwik Extension for Magento created by Adrian Speyer
+ * Get Piwik at http://www.piwik.org - Open source web analytics
+ *
+ * @category    Mage
+ * @package     Mage_PiwikAnalytics_Model_Observer
+ * @copyright   Copyright (c) 2012 Adrian Speyer. (http://www.adrianspeyer.com)
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+
+/**
+ * Piwik module observer
+ *
+ * @category   Mage
+ * @package    Mage_Piwik
+ */
+class Mage_PiwikAnalytics_Model_Observer
+{
+   
+
+    /**
+     * Add order information into Piwik block to render on checkout success pages
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function setPiwikAnalyticsOnOrderSuccessPageView(Varien_Event_Observer $observer)
+    {
+        $orderIds = $observer->getEvent()->getOrderIds();
+        if (empty($orderIds) || !is_array($orderIds)) {
+            return;
+        }
+        $block = Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('piwik_analytics');
+        if ($block) {
+            $block->setOrderIds($orderIds);
+        }
+    }
+
+
+   
+}
+   
+   
