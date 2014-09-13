e871f1f142e6a31e0f939c1833929f63a031bec3
Adrian <adriansonline@users.noreply.github.com>
2014-09-13 12:20:51 -0400

    Syntax fix for no category

--- app/code/community/PiwikMage/PiwikAnalytics/Block/Piwik.php
+++ app/code/community/PiwikMage/PiwikAnalytics/Block/Piwik.php
@@ -145,7 +145,7 @@ class PiwikMage_PiwikAnalytics_Block_Piwik extends Mage_Core_Block_Template
         $cats = $_product->getCategoryIds();
         //$category_id = $cats[0]; grabs first category
        // $category_id = below fix when no catgeories
-        if (isset($cats[0]) {$category_id = $cats[0]} else $category_id = null;
+        if (isset($cats[0])) {$category_id = $cats[0];} else {$category_id = null;}
         $category = Mage::getModel('catalog/category')->load($category_id);
         $category_name = $category->getName();
         $product = $currentproduct->getName();
